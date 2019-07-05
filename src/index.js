b183963f8067c6d651a6562acc879a44222a0abe
Yasuyuki <yas.la.pedrera78@gmail.com>
2019-07-05 16:22:48 +0200

    add appendChild

--- src/index.js
+++ src/index.js
@@ -13,13 +13,13 @@ class Character {
     /*
       キャラクターの名前、HP、MPを表示する。
     */
-    mainEl.innerHTML = `
-    <div>
+    const div = document.createElement('div');
+    div.innerHTML = `
       <p><b>名前: </b>${this.name}</p>
       <p><b>HP: </b>${this.hp}</p>
       <p><b>MP: </b>${this.mp}</p>
-    </div>
    `
+    mainEl.appendChild(div);
   }
 
   attack(defender) {
@@ -28,17 +28,26 @@ class Character {
       死んでいない場合は相手に与えたダメージを表示。
       相手が死んだ場合は相手に与えたダメージと死んだことを表示する。
     */
+    const div = document.createElement('div');
     if (this.hp <= 0) {
-      mainEl.innerHTML = `<p>${this.name}は故人です。攻撃できません。</p>`;
+      div.innerHTML = `<p>${this.name}は故人です。攻撃できません。</p>`;
+      mainEl.appendChild(div);
+
       return;
     }
     if (defender.hp <= 0) {
-      mainEl.innerHTML = `<p>${defender.name}は既に死亡しています。攻撃できません。</p>`;
+      div.innerHTML = `<p>${defender.name}は既に死亡しています。攻撃できません。</p>`;
+      mainEl.appendChild(div);
+
     } else {
       const damage = this.calcAttackDamage(defender);
-      mainEl.innerHTML = `<p>${defender.name}に${damage}のダメージ！</p>`;
+      div.innerHTML = `<p>${defender.name}に${damage}のダメージ！</p>`;
+      mainEl.appendChild(div);
+
       if (defender.hp <= damage) {
-        mainEl.innerHTML = `<p>${defender.name}に${damage}のダメージ！${defender.name}は天に召されました。</p>`;
+        div.innerHTML = `<p>${defender.name}に${damage}のダメージ！${defender.name}は天に召されました。</p>`;
+        mainEl.appendChild(div);
+
       }
       defender.hp -= damage;
     }
@@ -71,18 +80,28 @@ class Sorcerer extends Character {
       相手が死んでいる場合は回復が出来ないためその旨を表示する。
       MPが足りない場合はその旨を表示する。
     */
+    const div = document.createElement('div');
+
     if (this.hp <= 0) {
-      mainEl.innerHTML = `<p>${this.name}は故人です。動きません。</p>`;
+      div.innerHTML = `<p>${this.name}は故人です。動きません。</p>`;
+      mainEl.appendChild(div);
+
       return;
     }
     if (target.hp <= 0) {
-      mainEl.innerHTML = `<p>${target.name}は既に死亡しています。回復できません。</p>`;
+      div.innerHTML = `<p>${target.name}は既に死亡しています。回復できません。</p>`;
+      mainEl.appendChild(div);
+
     } else if (this.mp < 3) {
-      mainEl.innerHTML = '<p>MPが足りません。healできません。</p>';
+      div.innerHTML = '<p>MPが足りません。healできません。</p>';
+      mainEl.appendChild(div);
+
     } else {
       this.mp -= 3;
       target.hp += 15;
-      mainEl.innerHTML = `<p>${target.name}のHPを15回復！</p>`;
+      div.innerHTML = `<p>${target.name}のHPを15回復！</p>`;
+      mainEl.appendChild(div);
+
     }
   }
 
@@ -94,18 +113,30 @@ class Sorcerer extends Character {
       相手が死んでいる場合は攻撃が出来ないためその旨を表示する。
       MPが足りない場合はその旨を表示する。
     */
+    const div = document.createElement('div');
+
     if (this.hp <= 0) {
-      mainEl.innerHTML = `<p>${this.name}は死人です。動きません。</p>`;
+      div.innerHTML = `<p>${this.name}は死人です。動きません。</p>`;
+      mainEl.appendChild(div);
+
       return;
     }
     if (target.hp <= 0) {
-      mainEl.innerHTML = `<p>${target.name}は既に死亡しています。攻撃できません。</p>`;
+      div.innerHTML = `<p>${target.name}は既に死亡しています。攻撃できません。</p>`;
+      mainEl.appendChild(div);
+
     } else if (this.mp < 2) {
-      mainEl.innerHTML = '<p>MPが足りません。fireできません。</p>';
+      div.innerHTML = '<p>MPが足りません。fireできません。</p>';
+      mainEl.appendChild(div);
+
     } else {
-      mainEl.innerHTML = `<p>${target.name}に10のダメージ！</p>`;
+      div.innerHTML = `<p>${target.name}に10のダメージ！</p>`;
+      mainEl.appendChild(div);
+
       if (target.hp <= 10) {
         mainEl.innerHTML = `<p>${target.name}に10のダメージ！${target.name}は亡くなりました。</p>`;
+        mainEl.appendChild(div);
+
       }
       this.mp -= 2;
       target.hp -= 10;
